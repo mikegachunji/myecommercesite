fcb50f9d5f7f6fe5208680cf93847056c7f93f2d
mikegachunji <mikegachunji>
2018-05-11 21:49:51 +0300

    Changes to models.py

--- src/profiles/models.py
+++ src/profiles/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class profile(models.Model):
 	name = models.CharField(max_length=120)
+	description = models.TextField(null=True)
 
 	def __unicode__(self):
 		return self.name
