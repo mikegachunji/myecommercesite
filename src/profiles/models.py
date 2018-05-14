d8220e43023f321bd8aecce530fed03f752d62b7
mikegachunji <mikegachunji>
2018-05-14 18:08:52 +0300

    New changes

--- src/profiles/models.py
+++ src/profiles/models.py
@@ -3,7 +3,9 @@ from django.db import models
 # Create your models here.
 class profile(models.Model):
 	name = models.CharField(max_length=120)
-	description = models.TextField(null=True)
+	description = models.TextField(default='description default text')
+	location = models.CharField(max_length=120, default='my location default', blank=True, null=True)
+	job = models.CharField(max_length=120, null=True)
 
 	def __unicode__(self):
-		return self.name
+		return self.name 
