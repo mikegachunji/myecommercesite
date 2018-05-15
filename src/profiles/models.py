653e134e70b860ac59777f7819670be56aae6422
mikegachunji <mikegachunji>
2018-05-15 18:03:15 +0300

    Deleted location and job fields in profile model

--- src/profiles/models.py
+++ src/profiles/models.py
@@ -4,8 +4,7 @@ from django.db import models
 class profile(models.Model):
 	name = models.CharField(max_length=120)
 	description = models.TextField(default='description default text')
-	location = models.CharField(max_length=120, default='my location default', blank=True, null=True)
-	job = models.CharField(max_length=120, null=True)
+	
 
 	def __str__(self):
 		return self.name 
