a22e0ea636daac1b772924971a5c5bdb4875eb4c
mikegachunji <mikegachunji>
2018-05-15 17:51:58 +0300

    Changed unicode function to str

--- src/profiles/models.py
+++ src/profiles/models.py
@@ -7,5 +7,5 @@ class profile(models.Model):
 	location = models.CharField(max_length=120, default='my location default', blank=True, null=True)
 	job = models.CharField(max_length=120, null=True)
 
-	def __unicode__(self):
+	def __str__(self):
 		return self.name 
