460cd60e23380c410d8244d99bc7d61bd4105af8
mikegachunji <mikegachunji>
2018-05-25 15:27:17 +0300

    About page created

--- src/tryTen/urls.py
+++ src/tryTen/urls.py
@@ -22,7 +22,8 @@ from profiles import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
 ]
 
 if settings.DEBUG:
